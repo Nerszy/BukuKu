47d400fdd298bbb22a266ab326d3b527e7b677c0
yahyaadi <106127173+Nerszy@users.noreply.github.com>
2023-06-05 18:34:37 +0700

    Update database.js

--- cloud computing/config/database.js
+++ cloud computing/config/database.js
@@ -1,1 +1,2 @@
-exports.mongoURL =  "mongodb://localhost:27017/bukuKu"
+exports.mongoURL =  "mongodb://admin:admin123@ac-cp2ij3m-shard-00-00.cj1phjf.mongodb.net:27017,ac-cp2ij3m-shard-00-01.cj1phjf.mongodb.net:27017,ac-cp2ij3m-shard-00-02.cj1phjf.mongodb.net:27017/BukuKu?ssl=true&replicaSet=atlas-zganeq-shard-0&authSource=admin&retryWrites=true&w=majority"
+
